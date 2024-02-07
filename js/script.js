ecdb8bff60f567e49c983606ec0949eef3af59c4
jumalisilva <juliamalisilva@gmail.com>
2024-02-07 17:19:31 -0300

    JSON

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ console.log(stringJSON);
 // COnverter de volta para objeto
 const pessoa2 = JSON.parse(stringJSON);
 console.log(pessoa2);
+
+// JSON
+const meuJSON = 
+'{"nome": "Junior", "idade": 21, "skills":["HTML", "CSS", "JavaScript"]}';
+
+console.log(meuJSON);
+console.log(typeof meuJSON);
+
+// JSON da string e transforma objeto para JSON
+const meuObjeto = JSON.parse(meuJSON);
+console.log(meuObjeto);
+
+console.log(meuObjeto.nome);
+console.log(typeof meuObjeto);
+
+// JSON inválido
+const invalidoJSON = '{"nome": Junior, "idade": 21}';
+
+const novoInvalidoJSON = JSON.parse(invalidoJSON);
+console.log(novoInvalidoJSON);
